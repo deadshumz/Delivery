5ac28df7d30bb4daa7d03c41e12827f6a368d05a
Carlos Moerte <61364206+deadshumz@users.noreply.github.com>
2022-08-12 11:56:54 +0400

    Styling, image field

--- frontend/src/Components/RestaurantList.jsx
+++ frontend/src/Components/RestaurantList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import styles from './RestaurantList.module.css'
 
 export default function RestaurantList(props) {
 
@@ -18,10 +19,13 @@ export default function RestaurantList(props) {
                     }
 
                     return(
-                        <Col key={restaurant.id} md="6" xl="4" xxl="3" className='p-1'>
-                            <div className='border rounded p-3'>
-                                <h2>{restaurant.name}</h2>
-                                <p>Category: {restaurant_category}</p>
+                        <Col key={restaurant.id} md="6" xl="4" xxl="3" className={`p-1`}>
+                            <div className={`${styles.restaurant} border d-flex flex-column`}>
+                                <div>img</div>
+                                <div className='p-3'>
+                                    <h2 className={styles.title}>{restaurant.name}</h2>
+                                    <span className={`${styles.categoryBadge} mt-2`}>{restaurant_category}</span>
+                                </div>
                             </div>
                         </Col>
                     )
